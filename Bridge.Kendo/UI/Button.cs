bbb193cb0c025a03f25277480612518c3f4db092
Christopher Spence <ghaladen@gmail.com>
2017-02-16 12:45:41 -0600

    Added Button widget

---
+++ Bridge.Kendo/UI/Button.cs
@@ -0,0 +1,37 @@
+﻿using Bridge.Html5;
+using Bridge.jQuery2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Kendo.UI
+{
+    [External]
+    [ObjectLiteral]
+    public class ButtonOptions
+    {
+        public bool Enable { get; set; }
+        public string Icon { get; set; }
+        public string ImageUrl { get; set; }
+        public string SpriteCssClass { get; set; }
+
+        public Func<HtmlEventTarget> Click { get; set; }
+
+    }
+
+    [External]
+    public class Button : jQuery
+    {
+        public void Enable(bool toggle) { }
+
+        public void Bind(string name, Action<HtmlEventTarget> action) { }    
+    }
+
+    [External]
+    public static class jQueryExtensions
+    {
+        public static Button KendoButton(this jQuery jQuery) { return null; }
+    }
+}
