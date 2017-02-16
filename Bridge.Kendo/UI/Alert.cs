b4646dcf99c296a64ec8591f710c76c7071d291b
Christopher Spence <ghaladen@gmail.com>
2017-02-16 12:32:43 -0600

    Added Alert

---
+++ Bridge.Kendo/UI/Alert.cs
@@ -0,0 +1,22 @@
+﻿using Bridge.jQuery2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.Kendo.UI
+{
+    [External]
+    [ObjectLiteral]
+    public class AlertOptions
+    {
+        public string OkText { get; set; }
+    }
+
+    [External]    
+    public static class Alert 
+    {
+        public static jQuery KendoAlert(this jQuery jQuery, AlertOptions options = null) { return default(jQuery); }        
+    }
+}
