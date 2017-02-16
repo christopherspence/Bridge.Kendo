267427306a3dd7223708fc073c9f525a8df9ce55
Christopher Spence <ghaladen@gmail.com>
2017-02-16 12:49:55 -0600

    Added comments + fixes to Button widget

--- Bridge.Kendo/UI/Button.cs
+++ Bridge.Kendo/UI/Button.cs
@@ -12,11 +12,29 @@ namespace Bridge.Kendo.UI
     [ObjectLiteral]
     public class ButtonOptions
     {
+        /// <summary>
+        /// Indicates whether the Button should be enabled or disabled. By default, it is enabled, unless a disabled="disabled" attribute is detected.
+        /// </summary>
         public bool Enable { get; set; }
+
+        /// <summary>
+        /// Defines a name of an existing icon in the Kendo UI theme sprite. The icon will be applied as background image of a span element inside the Button. 
+        /// </summary>
         public string Icon { get; set; }
+
+        /// <summary>
+        /// Defines a URL, which will be used for an img element inside the Button. The URL can be relative or absolute. In case it is relative, it will be evaluated with relation to the web page URL.
+        /// </summary>
         public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Defines a CSS class (or multiple classes separated by spaces), which will be used for applying a background image to a span element inside the Button. In case you want to use an icon from the Kendo UI theme sprite background image, it is easier to use the icon property.
+        /// </summary>
         public string SpriteCssClass { get; set; }
 
+        /// <summary>
+        /// Fires when the Button is clicked with the mouse, touched on a touch device, or ENTER (or SPACE) is pressed while the Button is focused.
+        /// </summary>
         public Func<HtmlEventTarget> Click { get; set; }
 
     }
@@ -24,13 +42,15 @@ namespace Bridge.Kendo.UI
     [External]
     public class Button : jQuery
     {
-        public void Enable(bool toggle) { }
-
-        public void Bind(string name, Action<HtmlEventTarget> action) { }    
+        /// <summary>
+        /// Enables or disables the Button.
+        /// </summary>
+        /// <param name="toggle">Indicates whether the Button should be enabled or disabled. true and false arguments are accepted. If no argument is supplied, the Button will assume true and will be enabled.</param>
+        public void Enable(bool toggle = true) { }    
     }
 
     [External]
-    public static class jQueryExtensions
+    public static class ButtonExtensions
     {
         public static Button KendoButton(this jQuery jQuery) { return null; }
     }
